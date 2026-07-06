9c4a97503ca800fe4569918090d8d0923679f3bf
ankit-menon <ankitmnn7@gmail.com>
2026-07-06 11:48:48 +0530

    Update script.js

--- jss/script.js
+++ jss/script.js
@@ -14,4 +14,65 @@ intro.style.display="none";
 
 main.style.display="block";
 
-}
+};
+
+
+const container=document.getElementById("bubble-container");
+
+setInterval(()=>{
+
+const bubble=document.createElement("div");
+
+bubble.className="bubble";
+
+const size=10+Math.random()*20;
+
+bubble.style.width=size+"px";
+
+bubble.style.height=size+"px";
+
+bubble.style.left=Math.random()*100+"vw";
+
+bubble.style.animationDuration=(5+Math.random()*8)+"s";
+
+container.appendChild(bubble);
+
+setTimeout(()=>bubble.remove(),12000);
+
+},220);
+
+
+
+gsap.from(".title",{
+
+duration:2,
+
+y:-80,
+
+opacity:0,
+
+ease:"bounce"
+
+});
+
+
+gsap.from(".subtitle",{
+
+delay:.8,
+
+opacity:0,
+
+duration:2
+
+});
+
+
+gsap.from("#enter",{
+
+delay:1.5,
+
+scale:0,
+
+duration:1
+
+});
